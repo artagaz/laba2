bc2e6870ea4f1d1c83685fb9fc54f40565ac9cdd
Andrew <and.vaylin@gmail.com>
2024-09-24 14:49:13 +0700

    added 3-5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,7 +22,18 @@ public class Main {
 //
 //        System.out.println(distance(x1, y1, x2, y2));
 
-        //3
+        //3+4
+//        double a = scanner.nextDouble();
+//        int n = scanner.nextInt();
+//
+//        System.out.println(powerLoop(a, n));
+//        System.out.println(power(a, n));
+
+        //5
+        int n = scanner.nextInt();
+        int[] lastThree = {0,0,1};
+
+        System.out.println(tribonacci(n, lastThree));
     }
 
     public static boolean triangle(float a, float b, float c)
@@ -32,5 +44,39 @@ public class Main {
     public static double distance(float x1, float y1, float x2, float y2)
     {
         return Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    }
+
+    public static double powerLoop(double a, int n)
+    {
+        if (a>0 && n>=0) {
+            double result = 1.0;
+
+            for (int i = n; i > 0; i--) {
+                result *= a;
+            }
+
+            return result;
+        }else System.out.println("Wrong input");
+        return -1;
+    }
+
+    public static double power(double a, int n)
+    {
+        if (a<=0 || n<0) return -1;
+        if(n==0) return 1;
+        else return a*power(a, n-1);
+    }
+
+    public static int tribonacci(int n, int[] lastThree)
+    {
+        if (n<0) return -1;
+        else if(n==1||n==0) return 0;
+        else if(n>2)
+        {
+            int sum = IntStream.of(lastThree).sum();
+            lastThree[0] = lastThree[1];lastThree[1] = lastThree[2]; lastThree[2] = sum;
+            tribonacci(n-1, lastThree);
+        }
+        return lastThree[2];
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -16,4 +16,30 @@ class MainTest {
     void distance() {
         assertEquals(11, Math.floor(Main.distance(0,0, 8,8)));
     }
+
+    @Test
+    void powerLoop() {
+        assertEquals(32, Main.powerLoop(2,5));
+        assertEquals(1, Main.powerLoop(123, 0));
+        assertEquals(-1, Main.powerLoop(0, -123));
+    }
+
+    @Test
+    void power() {
+        assertEquals(32, Main.power(2,5));
+        assertEquals(1, Main.power(123, 0));
+        assertEquals(-1, Main.power(0, -123));
+    }
+
+    @Test
+    void tribonacci() {
+        int[] lastThree = {0,0,1};
+        assertEquals(0, Main.tribonacci(0, lastThree));
+        assertEquals(0, Main.tribonacci(1, lastThree));
+        assertEquals(1, Main.tribonacci(2, lastThree));
+        assertEquals(81, Main.tribonacci(10, lastThree));
+        assertEquals(-1, Main.tribonacci(-123123, lastThree));
+        lastThree = new int[]{1, 2, 4};
+        assertEquals(81, Main.tribonacci(7, lastThree));
+    }
 }
